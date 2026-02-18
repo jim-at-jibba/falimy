a296892a3b4439c9b8e5654be3c098e21fdb9002
James Best <jim@justjibba.net>
2026-02-18 16:40:18 +0000

    refactor(app): update app config

--- mobile/app.config.ts
+++ mobile/app.config.ts
@@ -2,6 +2,8 @@ import { ExpoConfig, ConfigContext } from "expo/config";
 
 export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
+  name: "falimy",
+  slug: "falimy",
   android: {
     ...config.android,
     config: {
